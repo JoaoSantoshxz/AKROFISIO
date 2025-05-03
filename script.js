ba5c089063b4f615d9a3b51d3912f30f92aa8b26
João Manoel dos Santos <joaomasantos09@gmail.com>
2025-05-03 16:12:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-
 const menuToggle = document.querySelector('.menu-toggle');
-const mainNav = document.querySelector('.main-nav');  
+const mainNav = document.querySelector('.main-nav');
 const navList = document.querySelector('#nav-list');
 
-
+// Alternar a classe "open" no menu
 menuToggle.addEventListener('click', () => {
-    
-    
-    const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
-
+    const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
     menuToggle.classList.toggle('open');
-
     mainNav.classList.toggle('open');
-
     menuToggle.setAttribute('aria-expanded', !isExpanded);
 });
 
+// Fechar menu ao clicar em um link
 navList.addEventListener('click', (event) => {
-    if (event.target.tagName === 'A' && mainNav.classList.contains('open')) {
-
+    if (event.target.tagName === 'A') {
         menuToggle.classList.remove('open');
         mainNav.classList.remove('open');
-        menuToggle.setAttribute('aria-expanded', false);
+        menuToggle.setAttribute('aria-expanded', 'false');
     }
+});
+
+document.addEventListener("scroll", () => {
+    document.querySelectorAll(".servico-item").forEach((item) => {
+        const posicao = item.getBoundingClientRect().top;
+        const alturaTela = window.innerHeight;
+
+        if (posicao < alturaTela - 100) {
+            item.classList.add("aparecer");
+        }
+    });
 });
